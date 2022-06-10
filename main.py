74624185a3d5936aafd202abaf443399104a134b
Hayato-7812 <haya12to17@gmail.com>
2022-06-10 17:44:11 +0900

    test setted msg

--- main.py
+++ main.py
@@ -45,13 +45,16 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
-    if(event.message.text == "test"):
+    if event.message.text == "test":
         line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text="succsses!"))
+            event.reply_token,
+            TextSendMessage(text="success!!"))
+
+    else:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text))
+
 
 
 
