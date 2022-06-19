20bbd93033dfeb0771dcb121c30b135b1018d25e
Hayato-7812 <haya12to17@gmail.com>
2022-06-19 18:53:34 +0900

    try add db items from LINE

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def handle_message(event):
         for item in get_items():
             print(item)
             columns_list.append(CarouselColumn(title=get_yt_info(item["uri"])["title"], 
-                                thumbnail_image_url=get_yt_info(item["uri"])["thumbnail_url"],
+                                # thumbnail_image_url=get_yt_info(item["uri"])["thumbnail_url"],
                                 text="recomended by: {}".format(item["rec_by"]),
                                 # text="recomended by: {} \ncomment: {}".format(item["rec_by"],item["comment"]),
                                 actions=[URIAction(label="Listen it",uri=item["uri"])]))
