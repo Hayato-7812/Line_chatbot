290aae0e5e2b8f9a197f562e75bd0ceb108eb227
hama <toshiyahama0310@gmail.com>
2022-06-20 22:21:00 +0900

    画像が表示されない不具合を修正した

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 
 from flask import Flask, request, abort
+#Flaskとhtmlの紐づけ(render_templateのimport)
+from flask import render_template
 # from db_handler import *
 
 from linebot import (
@@ -19,7 +21,7 @@ import os
 import time
 import re
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="/static")
 
 #環境変数取得
 YOUR_CHANNEL_ACCESS_TOKEN = "4gqA1mKoeRc57EWpN7ghpb8mE2KrDidW4FAsMWpkM1n8js/+XsDQ4JgHuD5Sht2uI/MIGHS1nwE+SVrjX4kSJuaODDYWaUAsmbg5RPAa+Yegd1NJZv69Apx8a6CaQDa6xmxfogroZ1vxuInnGockqwdB04t89/1O/w1cDnyilFU="
@@ -28,9 +30,9 @@ YOUR_CHANNEL_SECRET = "1dc09f06b2d107b6c1310c8507f07f41"
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
-@app.route("/")
-def Hello():
-    return "Hello!!"
+@app.route("/",methods=["GET", "POST"])
+def toppage():
+    return render_template("index.html")
 
 @app.route("/callback", methods=['POST'])
 def callback():
