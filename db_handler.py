5755ec6a0e0f51b1767a9184a73a0a0d76030836
Hayato-7812 <haya12to17@gmail.com>
2022-06-20 22:19:14 +0900

    change shared list order

--- db_handler.py
+++ db_handler.py
@@ -72,7 +72,7 @@ def get_items(conn,cur,tablename="A_MUSIC"):  #as dict in list
     for row in cur:
         # print("row:{}".format(row))
         result.append(dbvalue_urls(row[0],row[1],row[2],row[3],row[4],row[5]).to_dict())
-    return result
+    return result.reverse()
 
 @dbopen()
 def get_next_id(conn,cur,tablename="A_MUSIC"):
