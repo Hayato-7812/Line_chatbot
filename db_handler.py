d6a54836c77a4704d70858bdaaab8f454ee54905
hama <toshiyahama0310@gmail.com>
2022-07-08 00:58:28 +0900

    お問い合わせページ完了

--- db_handler.py
+++ db_handler.py
@@ -19,7 +19,7 @@ class dbvalue_base():
         return {k:str(getattr(self,k)) for k in self.attr}
 
 class dbvalue_urls(dbvalue_base):
-    def __init__(self,_id=-1,_rec_date="",_rec_by="",_title="",_uri="",_comment=None,_yourname="",_LINEname="",_mail="",_content="",_comment2=""):
+    def __init__(self,_id=-1,_rec_date="",_rec_by="",_title="",_uri="",_comment=None):
         self.id = _id
         self.rec_date = _rec_date
         self.rec_by = _rec_by
@@ -29,6 +29,8 @@ class dbvalue_urls(dbvalue_base):
         self.attr= ["id","rec_date","rec_by","title","uri","comment"]
         self.table="A_MUSIC"
 
+class Contact(dbvalue_base):
+    def __init__(self,_yourname="",_LINEname="",_mail="",_content="",_comment2=""):
         # db追加したい項目１:名前
         # db追加したい項目２:LINE名
         # db追加したい項目３:メールアドレスorLINEアカウント
@@ -70,7 +72,7 @@ def dbopen():
     return recv_func
  
 @dbopen()
-def add_item(conn,cur,obj:dbvalue_urls):
+def add_musicitem(conn,cur,obj:dbvalue_urls):
     # data = (obj.id, obj.uri,datetime.today(),obj.rec_by,obj.comment)
     # sql = 'INSERT INTO {} (ID,URL,USER,DATE,COMMENT) VALUES(?,?,?,?,?)'.format("urls")
     # cur.execute(sql,data)
@@ -81,24 +83,24 @@ def add_item(conn,cur,obj:dbvalue_urls):
     print("add item('ID={},REC_DATE={},REC_BY={},TITLE={},URI={},COMMENT={}) to {}".format(obj.id,obj.rec_date,obj.rec_by,obj.title,obj.uri,obj.comment,obj.table))
 
 @dbopen()
-def get_data(conn,cur,tablename = "A_MUSIC"):
+def get_musicdata(conn,cur,tablename = "A_MUSIC"):
     sql = 'SELECT * FROM {}'.format(tablename)
     cur.execute(sql)
 
 @dbopen()
-def get_items(conn,cur,tablename="A_MUSIC"):  #as dict in list 
+def get_musicitems(conn,cur,tablename="A_MUSIC"):  #as dict in list 
     # print("get db items")
     cur.execute("select * from {}".format(tablename))
     result = []
     for row in cur:
         # print("row:{}".format(row))
-        result.append(dbvalue_urls(row[0],row[1],row[2],row[3],row[4],row[5]).to_dict())
+        result.append(Contact(row[0],row[1],row[2],row[3],row[4],row[5]).to_dict())
     result.reverse()
     
     return result
 
 @dbopen()
-def get_next_id(conn,cur,tablename="A_MUSIC"):
+def get_music_next_id(conn,cur,tablename="A_MUSIC"):
     sql = 'SELECT count(*) FROM {}'.format(tablename)
     cur.execute(sql)
     result = cur.fetchall()
@@ -107,32 +109,35 @@ def get_next_id(conn,cur,tablename="A_MUSIC"):
     # print ('登録されている総レコード数 ==> ', record_max)
     return record_max+1
 
-# お問い合わせフォーム部分  関数は全て"2"を語尾につけた
+# <お問い合わせフォーム部分>
 
 @dbopen()
-def add_item2(conn,cur,obj:dbvalue_urls):
-    cur.execute("INSERT INTO {} (YOURNAME,LINENAME,MAIL,CONTENT,COMMENT2) VALUES ({}, '{}','{}','{}','{}','{}')".format(obj.table2,obj.yourname,obj.LINEname,obj.mail,obj.content,obj.comment2))
+def add_contactitem(conn,cur,obj:Contact):
+    # sql = 'INSERT INTO {} (YOURNAME,LINENAME,MAIL,CONTENT,COMMENT2) VALUES(?,?,?,?)'.format(obj.table2)
+    # data = (obj.yourname, obj.LINEname,obj.mail,obj.content,obj.comment2)
+    # cur.execute(sql, data)
+    cur.execute("INSERT INTO {} (YOURNAME,LINENAME,MAIL,CONTENT,COMMENT2) VALUES ('{}', '{}','{}','{}','{}')".format(obj.table2,obj.yourname,obj.LINEname,obj.mail,obj.content,obj.comment2))
     print("add item('YOURNAME={},LINENAME={},MAIL={},CONTENT={},COMMENT2={}) to {}".format(obj.yourname,obj.LINEname,obj.mail,obj.content,obj.comment2,obj.table2))
 
 @dbopen()
-def get_data2(conn,cur,tablename = "A_CONTACT"):
+def get_contactdata(conn,cur,tablename = "A_CONTACT"):
     sql = 'SELECT * FROM {}'.format(tablename)
     cur.execute(sql)
 
 @dbopen()
-def get_items2(conn,cur,tablename="A_CONTACT"):  #as dict in list 
+def get_contactitems(conn,cur,tablename="A_CONTACT"):  #as dict in list 
     # print("get db items")
     cur.execute("select * from {}".format(tablename))
     result = []
     for row in cur:
         # print("row:{}".format(row))
-        result.append(dbvalue_urls(row[8],row[9],row[10],row[11],row[12]).to_dict())
+        result.append(Contact(row[0],row[1],row[2],row[3],row[4]).to_dict())
     result.reverse()
     
     return result
 
 @dbopen()
-def get_next_id2(conn,cur,tablename2="A_CONTACT"):
+def get_contact_next_id(conn,cur,tablename2="A_CONTACT"):
     sql = 'SELECT count(*) FROM {}'.format(tablename2)
     cur.execute(sql)
     result = cur.fetchall()
@@ -141,7 +146,7 @@ def get_next_id2(conn,cur,tablename2="A_CONTACT"):
     # print ('登録されている総レコード数 ==> ', record_max)
     return record_max+1
 
-# 問い合わせ部分ここまで
+# <お問い合わせフォーム部分ここまで>
 
 if __name__ == "__main__":
     # input_url = "https://www.youtube.com/watch?v=n8cpqRJjumo"
